1ec5ff21e5f3e8f113a29485e91c09052faf0f35
eliscioter~ <attila433lehun@gmail.com>
2025-03-08 21:59:45 +0800

    fix: change way of adding application/json

--- src/main/java/com/eliscioter/terra/controllers/interfaces/IUserService.java
+++ src/main/java/com/eliscioter/terra/controllers/interfaces/IUserService.java
@@ -4,6 +4,9 @@ import com.eliscioter.terra.models.requests.CreateUserRequest;
 import com.eliscioter.terra.models.requests.UpdateUserRequest;
 import com.eliscioter.terra.models.wrapper.ResponseData;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
-
 @RequestMapping(value = "/user")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public interface IUserService {
 
     @GetMapping(value = "/fetchAll")
@@ -25,12 +29,12 @@ public interface IUserService {
     @GetMapping(value = "/{id}")
     ResponseEntity<ResponseData> getUser(@PathVariable UUID id);
 
-    @PostMapping(value = "/create", consumes = "application/json", produces = "application/json")
+    @PostMapping(value = "/create")
     ResponseEntity<ResponseData> createUser(@Valid @RequestBody CreateUserRequest createUserRequest);
 
-    @PatchMapping(value = "/update/{id}", consumes = "application/json", produces = "application/json")
+    @PatchMapping(value = "/update/{id}")
     ResponseEntity<ResponseData> updateUser(@PathVariable("id") UUID id, @Valid @RequestBody UpdateUserRequest updateUserRequest);
 
     @DeleteMapping(value = "/delete/{id}")
-    ResponseEntity<ResponseData> deleteUser(@PathVariable UUID id, @Valid @RequestBody CreateUserRequest deleteUserRequest);
+    ResponseEntity<ResponseData> deleteUser(@PathVariable UUID id);
 }
